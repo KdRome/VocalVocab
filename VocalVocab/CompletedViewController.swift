7dc993867e1c85d2f33e389da288d8469e1aa307
Roman <r.petliak12@gmail.com>
2023-11-11 00:45:05 -0500

    Basic Design

---
+++ VocalVocab/CompletedViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CompletedViewController.swift
+//  VocalVocab
+//
+//  Created by Roman Petlyak on 11/10/23.
+//
+
+import UIKit
+
+class CompletedViewController: UIViewController {
+    
+    override func viewWillAppear(_ animated: Bool){
+        super.viewDidLoad()
+        
+        
+    }
+    
+    
+}
